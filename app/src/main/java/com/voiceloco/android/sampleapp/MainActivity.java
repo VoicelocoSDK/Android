7d05a0dc0a3bf8495d52b2faaf0bd3ea780987ed
홍영택 <youngtaek@voiceloco.com>
2018-04-04 18:36:14 +0900

    add dependency, fcm files

--- app/src/main/java/com/voiceloco/android/sampleapp/MainActivity.java
+++ app/src/main/java/com/voiceloco/android/sampleapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.voiceloco.android.sampleapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.voiceloco.android.Register;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
